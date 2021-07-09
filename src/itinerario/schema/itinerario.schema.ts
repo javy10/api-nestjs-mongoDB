16e52e4e99ae6c9a8e73e4fd34950c92a4704d87
Lawliet <49668391+alramirez97@users.noreply.github.com>
2021-07-08 23:27:20 -0600

    Destino_id

--- src/itinerario/schema/itinerario.schema.ts
+++ src/itinerario/schema/itinerario.schema.ts
@@ -1,10 +1,11 @@
 import { time } from 'console';
 import *as mongoose from 'mongoose';
 import { destinoSchema } from 'src/destino/schema/destino.schema';
+import { origenSchema } from 'src/origen/schema/origen.schema';
 
 export const itinerarioSchema = new mongoose.Schema({
   Fecha: Date,
   Hora: String,
-  Destino_id_: { type: mongoose.Types.ObjectId },
+  Destino_id: { type: mongoose.Types.ObjectId },
   Origen_id: { type: mongoose.Types.ObjectId }
 });
