d8ec872dfe15f1ef15b0b886808b3b2384da415c
Lawliet <49668391+alramirez97@users.noreply.github.com>
2021-07-09 11:30:34 -0600

    Clientes documentacion separada

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Documentacion
+
+  const options = new DocumentBuilder()
+    .setTitle ('Documento de información del cliente')
+    .setDescription ('Se usa para agregar, eliminar y modificar información del cliente')
+    .setVersion('1.0')
+    .build();
+ 
+  const clientesDocument = SwaggerModule.createDocument(app, options, {
+         include: [ClientesModule], // módulos incluidos
+  });
+  SwaggerModule.setup('documentacion/clientes', app, clientesDocument);
 
   const config = new DocumentBuilder()
   .setTitle('API PROYECTO FINAL')
